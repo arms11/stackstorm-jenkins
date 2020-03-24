2c431f4830d74a6842f141d2b0e0613a06f5017c
Anand Patel <anandpatel@vmware.com>
2020-03-24 01:22:14 -0400

    Parse incoming JSON and initiate job

--- actions/build_job.py
+++ actions/build_job.py
@@ -6,7 +6,52 @@ class BuildProject(action.JenkinsBaseAction):
     def run(self, project, parameters=None, config_override=None):
         if config_override is not None:
             self.config_override(config_override)
-        name = parameters['alert']
-        print('Alert is ' + name)
-        return 1
-        # return self.jenkins.build_job(project, parameters)
+        if parameters is not None:
+            params_list = self.parse_data(parameters)
+            # Initiate job for each failing series (Server Name + Service Name)
+            for params in params_list:
+                job_params = { str(k):str(v) for k,v in params.items() }
+                queue_num = self.jenkins.build_job(project, job_params)
+                print('Remediation job {NUM} was queued for {SERVICE}'.format(NUM=queue_num, SERVICE=parameters['Service_Name']))
+            return (True, "Success")
+        else: 
+            return (False, "Alert data was not provided.")
+
+    # Parsing of Alert JSON to extract Server_Name, Service_Name, and Datacenter
+    def parse_data(self, paramJson):
+        params_list = []
+        alertData = self.sanitize_data(paramJson)
+        failing_series = alertData['failingSeries']
+        for series in failing_series:
+            job_params = {
+                'Server_Name': series[0], 
+                'Action_Type': 'Start'
+            }
+            
+            for pointTag in series[2]:
+                if "service_name=" in pointTag:
+                    job_params['Service_Name'] = pointTag.split("=")[1]
+                elif "datacenter=" in pointTag:
+                    job_params['Datacenter'] = pointTag.split("=")[1]
+            
+            params_list.append(job_params)
+
+        return params_list
+
+    
+    def sanitize_data(self, paramJson):
+        print('*****ORIGINAL*****')
+        print(paramJson)
+        print('*****ORIGINAL*****')
+        
+        alertData = {}
+        if isinstance(paramJson, str):
+            paramJson = paramJson.replace("u'", "'").replace("'", "\"")
+            print('*****MODIFIED*****')
+            print(paramJson)
+            print('*****MODIFIED*****')
+            alertData = json.loads(paramJson)
+        elif isinstance(paramJson, dict):
+            alertData = paramJson
+        
+        return alertData
